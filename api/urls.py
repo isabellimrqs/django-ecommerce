e63fc24f71d6eefaa3efd02f6b34ffb784e5e5f2
Isabelli Maciel Marques <44590917807@>
2024-03-12 11:20:42 -0300

    up

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('clientes', views.listar_clientes)
+    path('clientes', views.listar_clientes), 
+    path('usuarios', views.ClientesView.as_view())
 ]
